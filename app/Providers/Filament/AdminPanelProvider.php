60308655e61c78f2e2fab6e747ca8ec71763d8f6
Murat <muratgergov0820@gmail.com>
2024-05-29 14:54:21 +0300

    feat: Поработал с плагином добавляющий страницу настрек и инструментом по модификации этой страницы

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Models\User;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -20,6 +21,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentGeneralSettings\FilamentGeneralSettingsPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -31,7 +33,16 @@ class AdminPanelProvider extends PanelProvider
                 FilamentShieldPlugin::make(),
                 FilamentEnvEditorPlugin::make(),
                 SpatieLaravelTranslatablePlugin::make()
-                    ->defaultLocales(config('app.locales'))
+                    ->defaultLocales(config('app.locales')),
+                FilamentGeneralSettingsPlugin::make()
+                    ->setTitle('Настройки')
+                    ->setIcon('heroicon-o-adjustments-horizontal')
+                    ->canAccess(function () {
+                        /** @var User $user */
+                        $user = auth()->user();
+
+                        return $user->hasRole('super_admin');
+                    })
             ])
             ->id('admin')
             ->path('admin')
